46a6e376902ca8baa7ecff6efa7cdc7044b4cd74
azadei <azadei@code.edu.az>
2023-07-17 14:45:31 +0400

    1

---
+++ 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        int[] numbers = { -99, 1, 4, -5, 57, -23, 9, 2, -100 };
+
+        
+        var query = from num in numbers
+                    where num > 0 && num <= 100
+                    select num;
+
+       
+        Console.WriteLine("The numbers within the range of 1 to 11 are:");
+        foreach (int num in query)
+        {
+            Console.WriteLine(num + " ");
+        }
+    }
+}
